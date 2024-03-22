d46ab6f8e65a372a1ba781d842fce7dda980b69d
fudrk <164232396+fudrk@users.noreply.github.com>
2024-03-22 22:54:55 +0100

    requirements

---
+++ requirements.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Install necessary npm packages
+npm install colors readline-sync
+
+REM Run the JavaScript code
+node biggerfile.js.js
+
+REM Pause to keep the console window open
+pause
